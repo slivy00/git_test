aba7b91a94fc3a7e1efcf13390f24f4337130688
yaoshuilin <slivy12@163.com>
2021-04-29 17:13:15 +0800

    second commit

--- src/main/java/GitTest.java
+++ src/main/java/GitTest.java
@@ -2,5 +2,6 @@ public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello git");
         System.out.println("123");
+        System.out.println("hello git2");
     }
 }
